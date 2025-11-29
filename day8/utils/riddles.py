2848c08d133fccb50a1f3b5aff00846f1d6f6b1c
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2025-11-29 17:44:56 +0530

    Create riddles.py

---
+++ day8/utils/riddles.py
@@ -0,0 +1,14 @@
+RIDDLES = [
+    {
+        "question": "I speak without a mouth and hear without ears. I have nobody, but I come alive with wind. What am I?",
+        "answer": "echo",
+    },
+    {
+        "question": "What has keys but can't open locks?",
+        "answer": "piano",
+    },
+    {
+        "question": "The more of this there is, the less you see. What is it?",
+        "answer": "darkness",
+    },
+]
