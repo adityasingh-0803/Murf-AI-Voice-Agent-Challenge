1f4de8a315abc2a446bead8c3f75723ad095b8a9
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2025-11-29 17:44:25 +0530

    Create trivia_bank.py

---
+++ day8/utils/trivia_bank.py
@@ -0,0 +1,18 @@
+TRIVIA = [
+    {
+        "question": "What planet is known as the Red Planet?",
+        "answer": "mars",
+    },
+    {
+        "question": "How many continents are there on Earth?",
+        "answer": "7",
+    },
+    {
+        "question": "Who wrote the Harry Potter series?",
+        "answer": "j.k. rowling",
+    },
+    {
+        "question": "What is the largest ocean on Earth?",
+        "answer": "pacific ocean",
+    },
+]
