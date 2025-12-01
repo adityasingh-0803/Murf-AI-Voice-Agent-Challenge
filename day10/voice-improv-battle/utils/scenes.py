1ae9c37708d62cfbd2ce60b6ad5ff462da5cc24a
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2025-12-01 19:17:01 +0530

    Create scenes.py

---
+++ day10/voice-improv-battle/utils/scenes.py
@@ -0,0 +1,17 @@
+SCENES = [
+    {
+        "scene": "Two detectives arguing in a coffee shop about who gets the last donut.",
+        "characters": ["Detective Rao", "Detective Sharma"],
+        "mood": "dramatic"
+    },
+    {
+        "scene": "An astronaut and an alien trying to order pizza on Mars.",
+        "characters": ["Commander Vikram", "Zorlax"],
+        "mood": "chaotic"
+    },
+    {
+        "scene": "A magician who accidentally summoned a customer support agent.",
+        "characters": ["Arkos the Wizard", "HelpDeskBot-77"],
+        "mood": "comedic"
+    }
+]
